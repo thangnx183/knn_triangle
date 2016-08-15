4a1bc00a41868e8c475bed6a3efee87664b7dfda
thangnx183 <nxthang1831997@gmail.com>
2016-08-15 11:23:29 +0700

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,8 +1,6 @@
 import os
 import cv2
 import numpy as np
-#import os
-
 
 # return path of all image in folder
 def get_filepaths(directory):
@@ -34,27 +32,21 @@ def image_to_matrix(lis_dir):
     lis = np.array([])
     lis_test = np.array([])
 
-    #for i in range(len(lis_dir)/100):
     for i in range(7500):
-        #if i < len(lis_dir)*3/4/100:
         if i < 7500*3/4:
             matrix = convert_image(lis_dir[i])
             matrix = matrix.reshape((1,400))
-#kNN            matrix = np.insert(matrix,0,[1])
             lis = np.append(lis, matrix)
         else:
             matrix = convert_image(lis_dir[i])
             matrix = matrix.reshape((1,400))
-#kNN            matrix = np.insert(matrix,0,[1])
             lis_test = np.append(lis_test, matrix)
     x = 7500*3/4
     y = 7500 - 7500*3/4
     return lis,  x, lis_test, y
 
-
-
 def getdata():
-    #lis = os.listdir()
+    
     lis = get_filepaths('/home/thangnx/code/triangletest/triangle_competition/train/triangle')
     X1, len1, X1_test, len_test1 = image_to_matrix(lis)
     Y1 = np.ones((len1, 1))
@@ -66,16 +58,13 @@ def getdata():
     Y2_test = np.zeros((len_test2, 1))
 
     X = np.append(X1, X2)
-    X = X.reshape((len1 + len2, 400)) #knn
-    #X = np.matrix(X, dtype = float)
+    X = X.reshape((len1 + len2, 400)) 
 
     X_test = np.append(X1_test, X2_test)
-    X_test = X_test.reshape((len_test1 + len_test2, 400)) #kNN
+    X_test = X_test.reshape((len_test1 + len_test2, 400)) 
     X_test = np.matrix(X_test, dtype = float)
 
     Y = np.append(Y1, Y2)
-    #Y = Y.reshape((len1 + len2, 1))
-    #Y = np.matrix(Y.reshape((len1+len2,1)), dtype = float)
 
     Y_test = np.append(Y1_test, Y2_test)
     Y_test = np.matrix(Y_test.reshape((len_test1 + len_test2, 1)), dtype =float)
@@ -89,12 +78,11 @@ def get_final_test():
 
     for each in lisdir:
         matrix = convert_image('/home/thangnx/code/triangletest/triangle_competition/test/'+each)
-        matrix = matrix.reshape((1,400))
-#knn        matrix = np.insert(matrix,0,[1])
+        matrix = matrix.reshape((1,400)
         lis = np.append(lis, matrix)
-    #print len(lis)
-#knn    Xtest = np.matrix(lis.reshape((x, 401)))
+        
     Xtest = lis.reshape((x,400)) #knn
+    
     return Xtest, lisdir
 
 def result(h, lisdir):
